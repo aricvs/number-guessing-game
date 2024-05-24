ad7fd512f8365abc6e05c9ccccd4620659561965
Ariel Santos <aricvs@gmail.com>
2024-05-24 21:13:32 +0700

    Implement number picker function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import random
+
+
 def choose_mode():
     chosen_mode = input("Choose your mode, easy or hard (e/h): ").strip().lower()
     return 0 if chosen_mode == "e" else 1
+
+
+def pick_number():
+    goal_number = random.randint(1, 100)
+    return goal_number
