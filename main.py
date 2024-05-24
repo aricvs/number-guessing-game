f186c8ce0ecc7499a057118e99cf1bcbb5441fce
Ariel Santos <aricvs@gmail.com>
2024-05-24 21:33:36 +0700

    Implement number picker function

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ def choose_mode():
     return 10 if chosen_mode == "e" else 5
 
 
-def pick_number():
+def number_randomizer():
     goal_number = random.randint(1, 100)
     return goal_number
 
 
+def pick_number():
+    chosen_number = int(input("Choose a number from 1 to 100: "))
+    return chosen_number
+
+
 def game():
     lives = choose_mode()
-    goal_number = pick_number()
+    goal_number = number_randomizer()
+    # while lives > 0:
+
+
+print(pick_number())
