ff6a4b25f00181345268396e5c303115afc4a1d0
Ariel Santos <aricvs@gmail.com>
2024-05-24 21:29:56 +0700

    Start creating the main game function

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import random
 
 def choose_mode():
     chosen_mode = input("Choose your mode, easy or hard (e/h): ").strip().lower()
-    return 0 if chosen_mode == "e" else 1
+    return 10 if chosen_mode == "e" else 5
 
 
 def pick_number():
     goal_number = random.randint(1, 100)
     return goal_number
+
+
+def game():
+    lives = choose_mode()
+    goal_number = pick_number()
